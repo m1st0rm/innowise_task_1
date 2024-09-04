e19789a63106235308b1b1b33c616eb7ada7f545
m1st0rm <mishaboy333@gmail.com>
2024-09-04 10:21:25 +0300

    Added output_manager.py with implementation of output_json

---
+++ output_manager.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import os
+from typing import Any, List
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("logs/json_parser.log"),
+    ],
+)
+logger = logging.getLogger(__name__)
+
+
+def output_json(data: List[Any], task_number: str) -> str:
+    """
+    Converts the tasks data into a JSON-formatted string, writes it to a file,
+    and returns the absolute file path.
+
+    Args:
+        data (List[Any]): The data to be converted to JSON format.
+        task_number (str): The task number that provides the data.
+
+    Returns:
+        str: The absolute path to the written JSON file.
+    """
+    logger.info("Converting the %s data to JSON...", task_number)
+    try:
+        json_data = json.dumps(data, indent=4, ensure_ascii=False)
+        file_path = f"{task_number}_output.json"
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(json_data)
+        logger.info("Successfully converted the %s data to JSON.", task_number)
+        return os.path.abspath(file_path)
+    except (OSError, IOError) as e:
+        logging.error("Error writing the %s data JSON to file: %s", task_number, e)
+        raise
+    except json.JSONDecodeError as e:
+        logging.error("Error serializing the %s data to JSON: %s", task_number, e)
+        raise
